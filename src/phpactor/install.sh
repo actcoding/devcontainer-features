2d266b1eb6f45d0976e42f6bf7fff4774070a1d3
Axel Rindle <axel.rindle@act-coding.de>
2024-12-30 19:15:06 +0000

    feat: phpactor

---
+++ src/phpactor/install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+set -ex
+
+PHPACTOR_INSTALL_DIR="/opt/phpactor"
+
+git clone -b "${VERSION}" -- https://github.com/phpactor/phpactor "${PHPACTOR_INSTALL_DIR}"
+
+cd "${PHPACTOR_INSTALL_DIR}"
+
+composer install --no-dev
