82ca5b368886af2e9b98e2658417823bae50f275
Axel Rindle <mail@axelrindle.de>
2025-10-28 16:57:29 +0000

    refactor: download to tmp

--- src/git-delta/install.sh
+++ src/git-delta/install.sh
@@ -26,7 +26,7 @@ DELTA_VERSION="${VERSION:-0.18.2}"
 DELTA_ARCH="$(get_architecture)"
 DELTA_DOWNLOAD_URL="https://github.com/dandavison/delta/releases/download/${DELTA_VERSION}/git-delta_${DELTA_VERSION}_${DELTA_ARCH}.deb"
 
-DELTA_PACKAGE_FILE="git-delta.deb"
+DELTA_PACKAGE_FILE="/tmp/git-delta.deb"
 
 curl -fsSL "${DELTA_DOWNLOAD_URL}" -o "${DELTA_PACKAGE_FILE}"
 sudo dpkg -i "${DELTA_PACKAGE_FILE}"
